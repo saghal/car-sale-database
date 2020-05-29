d89cf128a8546be871b54987945ea9995054a6b5
saghal <mohammadsgh98@gmail.com>
2020-05-29 09:15:58 +0430

    insert data to city table

--- insertData.sql
+++ insertData.sql
@@ -13,5 +13,24 @@ INSERT INTO Province VALUES ('Tehran'),
                             ('Qom'),
                             ('Qazvin'),
                             ('Hormozgan')
+SELECT * FROM Province
 
-INSERT INTO City VALUES ()
+INSERT INTO City VALUES ('Tehran',1000),
+                        ('Gorgan',1001),
+                        ('Isfahan',1002),
+                        ('Shahin shahr',1002),
+                        ('Mobarakeh',1002),
+                        ('Mashhad',1003),
+                        ('Shiraz',1004),
+                        ('Ahvaz',1005),
+                        ('Bandar Mahshahr',1005),
+                        ('Hamedan',1006),
+                        ('Sari',1007),
+                        ('Amol',1007),
+                        ('Noor',1007),
+                        ('Rasht',1008),
+                        ('Fooman',1008),
+                        ('Karaj',1009),
+                        ('Qom',1010),
+                        ('')
+                        
