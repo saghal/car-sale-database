c88c796f4f9357376d5a5db791739df5763c1d13
saghal <mohammadsgh98@gmail.com>
2020-05-29 09:34:22 +0430

    insert data to branch table

--- insertData.sql
+++ insertData.sql
@@ -32,5 +32,37 @@ INSERT INTO City VALUES ('Tehran',1000),
                         ('Fooman',1008),
                         ('Karaj',1009),
                         ('Qom',1010),
-                        ('')
-                        
+                        ('Qazvin',1011),
+                        ('Bandar Abbas',1012)
+
+SELECT * FROM City                        
+
+INSERT INTO Branch VALUES ('Auto Gallery Nasiri',1000),
+                          ('Auto Gallery Saghali',1000),
+                          ('Auto Gallery Mirzaei',1000),
+                          ('Auto Gallery Kazemi',1000),
+                          ('Auto Gallery Abdi',1001),
+                          ('Auto Gallery Moghaddasi',1002),
+                          ('Auto Gallery Rezaei',1002),
+                          ('Auto Gallery Lazemi',1003),
+                          ('Auto Gallery Nosrati',1004),
+                          ('Auto Gallery Teimori',1005),
+                          ('Auto Gallery Yari',1005),
+                          ('Auto Gallery Hashemi',1006),
+                          ('Auto Gallery Biglari',1007),
+                          ('Auto Gallery Hajebi',1007),
+                          ('Auto Gallery Tavakoli',1008),
+                          ('Auto Gallery Lashkari',1009),
+                          ('Auto Gallery Kalbadi',1010),
+                          ('Auto Gallery Mohajer',1011),
+                          ('Auto Gallery Basiri',1012),
+                          ('Auto Gallery Hasanpour',1013),
+                          ('Auto Gallery Ghasemi',1014),
+                          ('Auto Gallery Ahmadi',1015),
+                          ('Auto Gallery Hadi',1015),
+                          ('Auto Gallery Parsa',1016),
+                          ('Auto Gallery Naeimi',1017),
+                          ('Auto Gallery Monsefi',1018)
+
+SELECT * FROM Branch        
+
