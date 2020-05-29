fb47a19517fd6051fe812f0c8d04f75d889e1314
saghal <mohammadsgh98@gmail.com>
2020-05-29 08:49:12 +0430

    add province and branch table

--- CreateTable.sql
+++ CreateTable.sql
@@ -1,6 +1,24 @@
-CREATE DATABASE carSaleDatabase
-CREATE TABLE City(
+Create DATABASE CarSaleDatabase
+USE CarSaleDatabase
+GO
+
+CREATE TABLE Province(
+    ProvinceID int PRIMARY KEY IDENTITY(1000,1),
+    ProvinceName VARCHAR(30) NOT NULL,
+
+)
+CREATE TABLE City
+(
     CityID int PRIMARY KEY IDENTITY(1000,1),
     CityName VARCHAR(30) NOT NULL,
     ProvinceID int NOT NULL,
+    FOREIGN KEY (ProvinceID) REFERENCES Province(ProvinceID)
+
+)
+
+CREATE TABLE Branch(
+    BranchID int PRIMARY KEY IDENTITY(1000,1),
+    BranchName VARCHAR(30) NOT NULL,
+    CityID int NOT NULL,
+    FOREIGN KEY (CityID) REFERENCES City(CityID)    
 )
